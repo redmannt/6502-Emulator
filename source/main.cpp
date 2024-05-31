b5f21d118c84654fb3fd41c068e6b7a007bcad06
Tim <timloris95@gmail.com>
2024-05-31 21:38:01 +0200

    Reverted main.cpp back to old version

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,15 @@ i32 main() {
     Memory mem;
 
     reset(&cpu, &mem);
+
+    cpu.X = 0xFF;
+    mem._data[MAIN_MEMORY_LOCATION]   = INS_LDA_ZPX;
+    mem._data[MAIN_MEMORY_LOCATION+1] = 0x80;
+    mem._data[0x007F] = 0x37;
+
     execute(&cpu, &mem, 4);
+
+    assert(cpu.A == 0x37);
 
     return 0;
 }
